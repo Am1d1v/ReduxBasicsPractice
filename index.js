1b843e40acd9e4a564a22d2770ebc9a9543a6aa5
Am1d1v <dimadiman1adima@gmail.com>
2024-01-22 19:53:28 +0300

    Redux Basics Pracitice. Combine Reducers

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const redux = require('redux');
 
-// Reducer
-const reducer = (state, action) => {
+
+// Initial State
+const initialState = 0;
+
+// Counter Reducer
+const counterReducer = (state = initialState, action) => {
 
     if(action.type === 'INCREMENT'){
         return state + 1;
@@ -10,8 +14,33 @@ const reducer = (state, action) => {
     return state;
 }
 
-// Store
-const store = redux.createStore(reducer);
+// ToDosReducer
+const todosReducer = (state = [], action) => {
+
+    if(action.type === 'ADD_TODO'){
+        return [...state, 'new objective']
+    }
+
+    return state;
+};
+
+
+// Combine Reducers
+const rootReducer = redux.combineReducers({
+    counter: counterReducer,
+    todos: todosReducer
+});
+
+// Combined Store
+const store = redux.createStore(rootReducer);
+
+
+// // Store
+// const store = redux.createStore(reducer);
 
 // State of store
-console.log(store.getState())
+console.log(store.getState());
+
+// Increase counter 
+store.dispatch({type: "INCREMENT"});
+console.log(store.getState());
